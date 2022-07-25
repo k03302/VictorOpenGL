98dfa5ad26e881f78de0c392a7a14b14cd52cb14
jobgrass <jobgrass@outlook.com>
2022-07-25 17:49:06 +0900

    ch7 going 3d

--- Victor/Default.vert
+++ Victor/Default.vert
@@ -6,10 +6,14 @@ layout (location = 2) in vec2 aTex;
 out vec3 color;
 out vec2 texCoord;
 uniform float scale;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
 
 void main()
 {
-	gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	//gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	gl_Position = proj * view * model * vec4(aPos, 1.0);
 	color = aColor;
 	texCoord = aTex;
 }
--- Victor/main.cpp
+++ Victor/main.cpp
@@ -17,9 +17,14 @@
 #include "VBO.h"
 #include "EBO.h"
 #include "Texture.h"
+#include "Camera.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
+
+const unsigned int width = 800;
+const unsigned int height = 800;
+
 
 /*
 // 삼각형 vertices
@@ -33,8 +38,8 @@ GLfloat vertices[] =
 	0.0f,   -0.5f * float(sqrt(3)) / 3,    0.0f,     0.8f, 0.3f,  0.02f   // Inner down
 };
 */
-
-// Vertices coordinates
+/*
+// 사각형 vertices
 GLfloat vertices[] =
 { //     COORDINATES     /        COLORS      /   TexCoord  //
 	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
@@ -49,7 +54,23 @@ GLuint indices[] =
 	0, 2, 1, // Upper triangle
 	0, 3, 2 // Lower triangle
 };
-
+*/
+GLfloat vertices[] = {
+//       COORDINATES       /     COLORS       /    TexCoord          //
+	0.5f,  0.0f,  0.5f,    0.83f, 0.70f, 0.44f,     5.0f, 0.0f,
+	0.5f,  0.0f, -0.5f,    0.83f, 0.70f, 0.44f,     0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,    0.83f, 0.70f, 0.44f,     5.0f, 0.0f,
+	-0.5f, 0.0f,  0.5f,    0.83f, 0.70f, 0.44f,     0.0f, 0.0f,
+	0.0f,  0.8f,  0.0f,    0.92f, 0.86f, 0.76f,     2.5f, 5.0f,
+};
+GLuint indices[] = {
+	0, 1, 2,
+	1, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
+};
 
 
 
@@ -64,7 +85,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "MyOpenGL", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(width, height, "MyOpenGL", nullptr, nullptr);
 	if (!window) {
 		std::cout << "failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -115,15 +136,51 @@ int main() {
 	Texture segu("segu256.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
 	segu.texUnit(shaderProgram, "tex0", 0);
 
+	float rotation = 0.0f;
+	float distance = 0.0f;
+	bool distanceToggle = true;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
+
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shaderProgram.Activate();
+		
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= 1.0f / 60.0f) {
+			rotation += 0.5f;
+			distance += 0.1f * (distanceToggle ? -1 : 1);
+			if (distance < -10) {
+				distanceToggle = false;
+			}
+			else if (distance > 0) {
+				distanceToggle = true;
+			}
+			prevTime = crntTime;
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+		
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, distance));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+
+		GLuint modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		GLuint viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		GLuint projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
 		// 셰이더프로그램의 uniform 변수를 변경하기 전에 셰이더프로그램을 실행해야 한다.
 		glUniform1f(uniID, 0.5f);
 		segu.Bind();
 		VAO1.Bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
